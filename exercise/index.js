ce917144c8705291d49cd719ed79229c0e99e601
Sasas28 <ajasterzamora28@gmail.com>
2023-09-05 20:56:53 +0800

    added array and loops session

--- exercise/index.js
+++ exercise/index.js
@@ -13,18 +13,46 @@
 // console.log(capitalize("wOrDs"));
 // console.log(lastletter("testing"));
 
-const userInput = parseInt(prompt("input a number"));
+// const userInput = parseInt(prompt("input a number"));
 
-for(let i = 1; i < userInput; i++) {
+// for(let i = 1; i < userInput; i++) {
 
-    if (i % 3 === 0) { 
-        console.log("fizz");
-    } else if (i % 5 === 0) {
-        console.log("buzz");
-    } else if (i % 3 === 0 && i % 5 === 0) {
-        console.log("fizzbuzz");
-    } else {
-        console.log(i);
-    }
+//     if (i % 3 === 0) { 
+//         console.log("fizz");
+//     } else if (i % 5 === 0) {
+//         console.log("buzz");
+//     } else if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("fizzbuzz");
+//     } else {
+//         console.log(i);
+//     }
 
-}
+// }
+
+let fruits = ["Apol", "Banana", "Orange"];
+
+fruits[0] = "Apple";
+let size = fruits.length; // to know the length of an array
+let removeLastElement = fruits.pop(); // to remove last element in an array
+let removeFirstElement = fruits.shift(); // to remove first element in an array
+let removeSpecificElement = fruits.splice(0, 1); // splice(start index to remove, how many to remove)
+let addElement = fruits.push("Mango"); // adding element in the last index
+let addElementStart = fruits.unshift("Blueberry"); // adding element in the first index
+let addSpecificElement = fruits.splice(1, 0, "Pineapple");
+
+const vegetables = ["tomato", "potato", "cucumber"];
+const meat = ["chicken", "pork", "beef"];
+
+let foods = fruits.concat(vegetables, meat); // merge an array
+
+const ages = [32, 33, 16, 40];
+
+for (let i = 0; i< foods.length; i++) {
+    // console.log(foods[i]);
+}// for (initialization; condition; iteration)
+
+foods.forEach((food) => console.log(food)); // another loop way
+
+for (let food of foods) {
+    //console.log(food);
+} // another way
